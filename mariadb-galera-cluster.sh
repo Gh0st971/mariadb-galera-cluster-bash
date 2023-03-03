70e496fe0b5386e1eb8c52df95bbc76d788e91f5
Gh0st971 <12710638+Gh0st971@users.noreply.github.com>
2023-03-03 17:58:44 +0100

    Update mariadb-galera-cluster.sh

--- mariadb-galera-cluster.sh
+++ mariadb-galera-cluster.sh
@@ -7,10 +7,10 @@
 ## Parameter setup ##
 
 CLUSTER_NAME=cluster_name
-MEMBER_NAME=cluster_member_name
-MEMBER_IP=10.0.0.4
+NODE_NAME=cluster_member_name
+NODE_IP=10.0.0.4
 
-CLUSTER_IP[0]=$MEMBER_IP
+CLUSTER_IP[0]=$NODE_IP
 CLUSTER_IP[1]=10.0.0.5
 CLUSTER_IP[2]=10.0.0.6
 # Add more if needed
@@ -45,18 +45,14 @@ binlog_format            = row
 default_storage_engine   = InnoDB
 innodb_autoinc_lock_mode = 2
 bind-address = 0.0.0.0
-wsrep_node_address=\"'$CLUSTER_MEMBER_NAME'\"' > $MARIADB_GALERA_CONF
+wsrep_node_address=\"'$NODE_IP'\"
+wsrep_node_name=\"'$NODE_NAME'\"' > $MARIADB_GALERA_CONF
 
 sudo galera_new_cluster
 sudo systemctl restart mariadb 
 
-
-
-
-
 wsrep_node_address=\"'$IP'\"
 wsrep_node_name=\"'$IP'\"' > /etc/mysql/conf.d/cluster.cnf"
-
 
 sudo service mysql stop"
 sudo service mysql start --wsrep-new-cluster
